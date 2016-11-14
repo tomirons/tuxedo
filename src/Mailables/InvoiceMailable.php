1dd6dbde416ba6ec495e5a8d609b3d91454bc82e
Tom Irons <tom8004@hotmail.com>
2016-11-14 11:32:15 -0500

    Add more properties and doc blocks

--- src/Mailables/InvoiceMailable.php
+++ src/Mailables/InvoiceMailable.php
@@ -16,14 +16,73 @@ class InvoiceMailable extends TuxedoMailable
      */
     public $view = 'tuxedo::templates.invoice';
 
+    /**
+     * The user's name
+     *
+     * @var string|null
+     */
+    public $name = null;
+
+    /**
+     * The invoice Number
+     *
+     * @var string|null
+     */
+    public $invoiceNumber = null;
+
+    /**
+     * The invoice date
+     *
+     * @var string|null
+     */
+    public $date = null;
+
+    /**
+     * The invoice total
+     *
+     * @var string|null
+     */
+    public $total = null;
+
+    /**
+     * The items that are on the invoice
+     *
+     * @var array
+     */
     public $items = [];
 
+    /**
+     * Add an item to the invoice
+     *
+     * @param string $name
+     * @param string|integer $price
+     * @return $this
+     */
     public function item($name, $price)
     {
         $this->items[] = [
             'name' => $name,
             'price' => $price
         ];
+
+        $this->total += $price;
+
+        return $this;
+    }
+
+    /**
+     * Set the information for the invoice
+     *
+     * @param string $name
+     * @param string|integer $number
+     * @param string $date
+     * @return $this
+     */
+    public function information($name, $number, $date)
+    {
+        $this->name = $name;
+        $this->invoiceNumber = $number;
+        $this->date = $date;
 
         return $this;
     }
