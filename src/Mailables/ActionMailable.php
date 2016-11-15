bdec36840275671bca7a066c35f8f97b11498a15
Tom Irons <tom8004@hotmail.com>
2016-11-15 13:51:24 -0500

    Move properties and methods

--- src/Mailables/ActionMailable.php
+++ src/Mailables/ActionMailable.php
@@ -2,10 +2,10 @@
 
 namespace TomIrons\Tuxedo\Mailables;
 
+use Illuminate\Mail\Mailable;
 use TomIrons\Tuxedo\Message;
-use TomIrons\Tuxedo\TuxedoMailable;
 
-class ActionMailable extends TuxedoMailable
+class ActionMailable extends Mailable
 {
     use Message;
 
@@ -24,11 +24,69 @@ class ActionMailable extends TuxedoMailable
     protected $textView = 'tuxedo::templates.action-plain';
 
     /**
+     * The "level" of the message (info, success, error).
+     *
+     * @var string
+     */
+    public $level = 'info';
+
+    /**
      * The header for the message.
      *
      * @var string|null
      */
     public $header = null;
+
+    /**
+     * The text / label for the action.
+     *
+     * @var string
+     */
+    public $actionText;
+
+    /**
+     * The action URL.
+     *
+     * @var string
+     */
+    public $actionUrl;
+
+    /**
+     * Indicate that the message gives information about a successful operation.
+     *
+     * @return $this
+     */
+    public function success()
+    {
+        $this->level = 'success';
+
+        return $this;
+    }
+
+    /**
+     * Indicate that the message gives information about an error.
+     *
+     * @return $this
+     */
+    public function error()
+    {
+        $this->level = 'error';
+
+        return $this;
+    }
+
+    /**
+     * Set the "level" of the message (success, error, etc.).
+     *
+     * @param  string $level
+     * @return $this
+     */
+    public function level($level)
+    {
+        $this->level = $level;
+
+        return $this;
+    }
 
     /**
      * Set the header for the message
@@ -39,6 +97,38 @@ class ActionMailable extends TuxedoMailable
     public function header($header)
     {
         $this->header = $header;
+
+        return $this;
+    }
+
+    /**
+     * Configure the "call to action" button.
+     *
+     * @param  string  $text
+     * @param  string  $url
+     * @return $this
+     */
+    public function action($text, $url)
+    {
+        $this->actionText = $text;
+        $this->actionUrl = $url;
+
+        return $this;
+    }
+
+    /**
+     * Add a line of text to the message.
+     *
+     * @param  string|array $line
+     * @return $this
+     */
+    public function line($line)
+    {
+        if (! $this->actionText) {
+            $this->introLines[] = $this->formatLine($line);
+        } else {
+            $this->outroLines[] = $this->formatLine($line);
+        }
 
         return $this;
     }
--- src/Message.php
+++ src/Message.php
@@ -9,13 +9,6 @@ namespace TomIrons\Tuxedo;
 trait Message
 {
     /**
-     * The "level" of the notification (info, success, error).
-     *
-     * @var string
-     */
-    public $level = 'info';
-
-    /**
      * The message's greeting.
      *
      * @var string|null
@@ -37,60 +30,9 @@ trait Message
     public $outroLines = [];
 
     /**
-     * The text / label for the action.
-     *
-     * @var string
-     */
-    public $actionText;
-
-    /**
-     * The action URL.
-     *
-     * @var string
-     */
-    public $actionUrl;
-
-    /**
-     * Indicate that the notification gives information about a successful operation.
-     *
-     * @return $this
-     */
-    public function success()
-    {
-        $this->level = 'success';
-
-        return $this;
-    }
-
-    /**
-     * Indicate that the notification gives information about an error.
-     *
-     * @return $this
-     */
-    public function error()
-    {
-        $this->level = 'error';
-
-        return $this;
-    }
-
-    /**
-     * Set the "level" of the notification (success, error, etc.).
-     *
-     * @param  string  $level
-     * @return $this
-     */
-    public function level($level)
-    {
-        $this->level = $level;
-
-        return $this;
-    }
-
-    /**
      * Set the greeting of the message.
      *
-     * @param  string  $greeting
+     * @param  string $greeting
      * @return $this
      */
     public function greeting($greeting)
@@ -103,27 +45,12 @@ trait Message
     /**
      * Add a line of text to the message.
      *
-     * @param  string  $line
+     * @param  string|array $line
      * @return $this
      */
     public function line($line)
     {
-        return $this->newLine($line);
-    }
-
-    /**
-     * Add a line of text to the message.
-     *
-     * @param  string|array  $line
-     * @return $this
-     */
-    public function newLine($line)
-    {
-        if (! $this->actionText) {
-            $this->introLines[] = $this->formatLine($line);
-        } else {
-            $this->outroLines[] = $this->formatLine($line);
-        }
+        $this->outroLines[] = $this->formatLine($line);
 
         return $this;
     }
@@ -131,8 +58,8 @@ trait Message
     /**
      * Format the given line of text.
      *
-     * @param  string|array  $line
-     * @return string
+     * @param  string|array $line
+     * @return $this
      */
     protected function formatLine($line)
     {
@@ -141,20 +68,5 @@ trait Message
         }
 
         return trim(implode(' ', array_map('trim', preg_split('/\\r\\n|\\r|\\n/', $line))));
-    }
-
-    /**
-     * Configure the "call to action" button.
-     *
-     * @param  string  $text
-     * @param  string  $url
-     * @return $this
-     */
-    public function action($text, $url)
-    {
-        $this->actionText = $text;
-        $this->actionUrl = $url;
-
-        return $this;
     }
 }
