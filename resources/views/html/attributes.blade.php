afd96118acbfd74ed868f5ac415bab0a631e6167
Tom Irons <tom8004@hotmail.com>
2016-12-15 13:54:31 -0500

    Begin new invoice template

---
+++ resources/views/html/attributes.blade.php
@@ -0,0 +1,14 @@
+<table class="attribute-list" width="100%" cellpadding="0" cellspacing="0">
+    <tr>
+        <td class="attribute-list-container">
+            <table width="100%" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td class="attribute-list-item"><strong>Amount Due:</strong> ${{ number_format($total, 2, '.', ',') }}</td>
+                </tr>
+                <tr>
+                    <td class="attribute-list-item"><strong>Due By:</strong> {{ $due }}</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
