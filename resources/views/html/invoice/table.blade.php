246ab8bfac7b0387f976eac6aa606962fb18746a
Huludini <tom8004@hotmail.com>
2017-01-22 01:05:48 -0500

    Add checks to remove the extra borders

--- resources/views/html/invoice/table.blade.php
+++ resources/views/html/invoice/table.blade.php
@@ -16,37 +16,37 @@
                         <p class="align-right">Amount</p>
                     </th>
                 </tr>
-                @foreach($data['items'] as $item)
+                @foreach ($data['items'] as $item)
                     <tr>
                         <td width="80%" class="purchase_item">{{ $item[$data['keys']['name']] }}</td>
                         <td class="align-right" width="20%" class="purchase_item">{{ $item[$data['keys']['price']] }}</td>
                     </tr>
                 @endforeach
-                @if ($data['shipping'])
+                @if ($data['shipping'] > 0)
                     <tr>
                         <td width="80%" class="purchase_footer" valign="middle">
-                            <p class="purchase_total purchase_total--label">Shipping</p>
+                            <p class="purchase_shipping purchase_shipping--label">Shipping</p>
                         </td>
                         <td width="20%" class="purchase_footer" valign="middle">
-                            <p class="purchase_total">{{ $data['shipping'] }}</p>
+                            <p class="purchase_shipping">{{ $data['shipping'] }}</p>
                         </td>
                     </tr>
                 @endif
-                @if ($data['tax'])
+                @if ($data['tax'] > 0)
                     <tr>
-                        <td width="80%" class="purchase_footer" valign="middle">
-                            <p class="purchase_total purchase_total--label">Tax</p>
+                        <td width="80%" class="{{ !$data['shipping'] ? 'purchase_footer' : null }}" valign="middle">
+                            <p class="purchase_tax purchase_tax--label">Tax</p>
                         </td>
-                        <td width="20%" class="purchase_footer" valign="middle">
-                            <p class="purchase_total">{{ $data['tax'] }}</p>
+                        <td width="20%" class="{{ !$data['shipping'] ? 'purchase_footer' : null }}" valign="middle">
+                            <p class="purchase_tax">{{ $data['tax'] }}</p>
                         </td>
                     </tr>
                 @endif
                 <tr>
-                    <td width="80%" class="purchase_footer" valign="middle">
+                    <td width="80%" class="{{ !$data['tax'] && !$data['shipping'] ? 'purchase_footer' : null }}" valign="middle">
                         <p class="purchase_total purchase_total--label">Total</p>
                     </td>
-                    <td width="20%" class="purchase_footer" valign="middle">
+                    <td width="20%" class="{{ !$data['tax'] && !$data['shipping'] ? 'purchase_footer' : null }}" valign="middle">
                         <p class="purchase_total">{{ $data['total'] }}</p>
                     </td>
                 </tr>
