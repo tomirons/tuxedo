65b32df9de1f73f842998a6d21d746d172fde5f2
Huludini <tom8004@hotmail.com>
2017-01-31 19:31:01 -0500

    add more methods for setting the type

--- src/Mailables/AlertMailable.php
+++ src/Mailables/AlertMailable.php
@@ -55,4 +55,44 @@ class AlertMailable extends Mailable
 
         return $this;
     }
+
+    /**
+     * Set the type of the alert to "info"
+     *
+     * @return $this
+     */
+    public function info()
+    {
+        return $this->type('info');
+    }
+
+    /**
+     * Set the type of the alert to "warning"
+     *
+     * @return $this
+     */
+    public function warning()
+    {
+        return $this->type('warning');
+    }
+
+    /**
+     * Set the type of the alert to "success"
+     *
+     * @return $this
+     */
+    public function success()
+    {
+        return $this->type('success');
+    }
+
+    /**
+     * Set the type of the alert to "error"
+     *
+     * @return $this
+     */
+    public function error()
+    {
+        return $this->type('error');
+    }
 }
