5cf05555792602c19e5890b49bd1099437dfa20f
Huludini <tom8004@hotmail.com>
2017-01-31 21:47:07 -0500

    fix the plain email text

--- resources/views/templates/invoice.blade.php
+++ resources/views/templates/invoice.blade.php
@@ -5,7 +5,8 @@
 Thanks for using {{ config('app.name') }}. This is an invoice for your recent purchase.
 
 @component('mail::invoice.attributes', ['total' => $tableData['total'], 'dueDate' => $dueDate])
-
+# Amount Due: {{ $tableData['total'] }} <br>
+# Due By: {{ $dueDate }}
 @endcomponent
 
 @component('mail::button', ['url' => $url, 'color' => 'green'])
@@ -13,7 +14,18 @@ Pay this invoice
 @endcomponent
 
 @component('mail::invoice.table', ['data' => $tableData])
-
+| Description | Amount |
+| ----------- | ------ |
+@foreach($tableData['items'] as $item)
+| {{ $item[$tableData['keys']['name']] }} | {{ $item[$tableData['keys']['price']] }} |
+@endforeach
+@if ($tableData['shipping'] > 0)
+| Shipping | {{ $tableData['shipping'] }} |
+@endif
+@if ($tableData['tax'] > 0)
+| Tax | {{ $tableData['tax'] }} |
+@endif
+| Total | {{ $tableData['total'] }} |
 @endcomponent
 
 Regards, <br>
