4b981b17298caf226792afa5a6b2c97ea61f163c
Huludini <tom.irons@hotmail.com>
2017-03-17 23:52:09 -0400

    make more traits

---
+++ src/Traits/HasLine.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace TomIrons\Tuxedo\Traits;
+
+trait HasLine
+{
+    /**
+     * The "intro" lines of the message.
+     *
+     * @var array
+     */
+    public $introLines = [];
+
+    /**
+     * The "outro" lines of the message.
+     *
+     * @var array
+     */
+    public $outroLines = [];
+
+    /**
+     * Add a line of text to the message.
+     *
+     * @param string|array $line
+     *
+     * @return $this
+     */
+    public function line($line)
+    {
+        $this->outroLines[] = $this->formatLine($line);
+
+        return $this;
+    }
+
+    /**
+     * Format the given line of text.
+     *
+     * @param string|array $line
+     *
+     * @return $this
+     */
+    protected function formatLine($line)
+    {
+        if (is_array($line)) {
+            return implode(' ', array_map('trim', $line));
+        }
+
+        return trim(implode(' ', array_map('trim', preg_split('/\\r\\n|\\r|\\n/', $line))));
+    }
+}
