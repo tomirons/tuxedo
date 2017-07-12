2a790283bc0ea17304c7e936a286b24c2808d4b2
tomirons <tom.irons@hotmail.com>
2017-07-12 16:06:07 -0400

    create class to store methods for all mailables

---
+++ src/TuxedoMessage.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace TomIrons\Tuxedo;
+
+class TuxedoMessage extends Mailable
+{
+    /**
+     * The message's greeting.
+     *
+     * @var string
+     */
+    public $greeting;
+
+    /**
+     * The message's salutation.
+     *
+     * @var string
+     */
+    public $salutation;
+
+    /**
+     * Set the greeting of the message.
+     *
+     * @param string $greeting
+     *
+     * @return $this
+     */
+    public function greeting($greeting)
+    {
+        $this->greeting = $greeting;
+
+        return $this;
+    }
+
+    /**
+     * Set the salutation of the message.
+     *
+     * @param  string  $salutation
+     * @return $this
+     */
+    public function salutation($salutation)
+    {
+        $this->salutation = $salutation;
+
+        return $this;
+    }
+}
