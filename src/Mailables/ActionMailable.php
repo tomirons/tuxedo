380629c09380dd216ba8b44896a6580d5ad37b38
tomirons <tom.irons@hotmail.com>
2017-07-12 15:27:23 -0400

    remove the 'line' method

--- src/Mailables/ActionMailable.php
+++ src/Mailables/ActionMailable.php
@@ -20,22 +20,4 @@ class ActionMailable extends Mailable
      * @var string
      */
     public $markdown = 'tuxedo::templates.action';
-
-    /**
-     * Add a line of text to the message.
-     *
-     * @param string|array $line
-     *
-     * @return $this
-     */
-    public function line($line)
-    {
-        if (!$this->actionText) {
-            $this->introLines[] = $this->formatLine($line);
-        } else {
-            $this->outroLines[] = $this->formatLine($line);
-        }
-
-        return $this;
-    }
 }
